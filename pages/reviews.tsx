82e3205f3dbdd4c200b4833c7435c1a76990b67b
Nico <cinosotelo@gmail.com>
2026-06-14 12:53:48 -0400

    Added reviews template

---
+++ pages/reviews.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+interface HomeProps {
+  openQuote: () => void;
+}
+
+export default function Reviews({ openQuote }: HomeProps) {
+  return (
+    <div>
+     
+     <h1>hello ppls</h1>
+     
+
+   
+    
+    </div>
+  );
+}
